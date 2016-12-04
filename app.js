cdb26d967ad5c7184449deee09457ef2c9ee0ca2
kannokanno <akapanna@gmail.com>
2016-12-04 13:17:46 +0900

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+function main() {
+  var tweets = getAllTweets().filter(availableTweet);
+  postTweets(tweets);
+}
+
+function getTweets() {
+}
+
+function availableTweet(tweet) {
+}
+
+function postTweets(tweets) {
+}
